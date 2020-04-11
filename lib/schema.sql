f320ba308a175133fd58c5b11ef2333da18c709d
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-11 20:37:16 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,34 @@ create table series (
   author_id integer,
   subgenre_id integer
 );
+
+create table subgenres (
+  id INTEGER PRIMARY KEY,
+  name text
+);
+
+create table authors (
+  id INTEGER PRIMARY KEY,
+  name text
+);
+
+create table books (
+  id INTEGER PRIMARY KEY,
+  title text,
+  year integer,
+  series_id integer
+);
+
+create table characters (
+  id INTEGER PRIMARY KEY,
+  name text,
+  motto text,
+  species text,
+  author_id integer
+);
+
+create table character_books (
+  id INTEGER PRIMARY KEY,
+  book_id integer,
+  character_id integer
+);
