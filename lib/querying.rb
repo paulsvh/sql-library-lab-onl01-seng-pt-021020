bcc53f242568cc7c023eb0bf6792fcf7dfdf87d9
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-11 21:09:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "select series.title from series join books on books.series_id = series.id join character_books on character_books.book_id = books.id join characters on character_books.character_id = characters.id where characters.species = 'human' order by count(*) desc limit 1;"
+  "select series.title from series join books on books.series_id = series.id join character_books on character_books.book_id = books.id join characters on character_books.character_id = characters.id where characters.species = 'human' group by series.title order by count(*) desc limit 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
